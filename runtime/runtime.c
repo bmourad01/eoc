ae5550ebef22b2c27e92215b67fcfa786fddac6e
bmourad01 <Benjamin_Mourad@student.uml.edu>
2021-01-14 13:09:26 -0500

    add runtime.c

---
+++ runtime/runtime.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int64_t read_int()
+{
+  char buf[64];
+  
+  fgets(buf, sizeof(buf), stdin);
+  buf[strcspn(buf, "\n")] = '\0';
+
+  return atoll(buf);
+}
