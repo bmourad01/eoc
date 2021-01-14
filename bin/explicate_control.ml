a2a5d21f920fab1a88c4fdc750db98771517e997
bmourad01 <Benjamin_Mourad@student.uml.edu>
2021-01-13 20:18:50 -0500

    etc

--- bin/explicate_control.ml
+++ bin/explicate_control.ml
@@ -3,6 +3,7 @@ open Core_kernel
 let () =
   let prog = Eoc.Parse_r.parse Sys.argv.(1) in
   print_endline
-    Eoc.C.(
-      to_string
-        (explicate_control (Eoc.R_anf.resolve_complex Eoc.R.(uniquify prog))))
+    Eoc.(
+      C.(
+        to_string
+          (explicate_control (R_anf.resolve_complex R.(uniquify prog)))))
