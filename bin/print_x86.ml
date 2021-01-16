6484414a045fb696f5d1ac9deea3df554b490072
bmourad01 <Benjamin_Mourad@student.uml.edu>
2021-01-16 16:37:54 -0500

    use pipe

--- bin/print_x86.ml
+++ bin/print_x86.ml
@@ -1,14 +1,9 @@
 open Core_kernel
 
 let () =
-  let p = Eoc.Parse_r.parse Sys.argv.(1) in
-  let p = Eoc.R.uniquify p in
-  let p = Eoc.R_anf.resolve_complex p in
-  let p = Eoc.C.explicate_control p in
-  let p = Eoc.X.select_instructions p in
-  let p = Eoc.X.uncover_live p in
-  let p = Eoc.X.build_interference p in
-  let p = Eoc.X.allocate_registers p in
-  let p = Eoc.X.remove_jumps p in
-  let p = Eoc.X.patch_instructions p in
-  print_endline (Eoc.X.to_string p)
+  Eoc.Parse_r.parse Sys.argv.(1)
+  |> Eoc.R.uniquify |> Eoc.R_anf.resolve_complex |> Eoc.C.explicate_control
+  |> Eoc.X.select_instructions |> Eoc.X.uncover_live
+  |> Eoc.X.build_interference |> Eoc.X.allocate_registers
+  |> Eoc.X.remove_jumps |> Eoc.X.patch_instructions |> Eoc.X.to_string
+  |> print_endline
