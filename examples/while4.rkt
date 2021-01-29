33a161361bf286bd61cb40fbd54d477f8b1fe1da
bmourad01 <Benjamin_Mourad@student.uml.edu>
2021-01-29 18:05:00 -0500

    make the example sillier

--- examples/while4.rkt
+++ examples/while4.rkt
@@ -1,8 +1,8 @@
 (let ([x 0] [y 0])
   (begin
     (while (< x 666)
-      (let ([v (vector x (vector x (vector x)))])
+      (let ([v (vector (void) (vector (void) (vector x)))])
         (begin
-          (set! y (vector-ref (vector-ref v 1) 0))
+          (set! y (vector-ref (vector-ref (vector-ref v 1) 1) 0))
           (set! x (+ x 1)))))
     (+ y 1)))
