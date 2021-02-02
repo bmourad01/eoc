a54d729f7f6dfd17019d8ca5dcaff41da6d29a44
bmourad01 <Benjamin_Mourad@student.uml.edu>
2021-02-01 21:10:52 -0500

    even weirder

--- examples/print1.rkt
+++ examples/print1.rkt
@@ -1,14 +1,13 @@
 (let ([n (read)]
-      [i 1]
-      [v (vector 0 1)]
+      [v (vector 1 0 1)]
       [f (lambda: () : Void
-           (let ([sum (+ (vector-ref v 0)
-                         (vector-ref v 1))])
+           (let ([sum (+ (vector-ref v 1)
+                         (vector-ref v 2))])
              (begin
-               (vector-set! v 0 (vector-ref v 1))
-               (vector-set! v 1 sum))))])
-  (while (<= i n)
+               (vector-set! v 1 (vector-ref v 2))
+               (vector-set! v 2 sum)
+               (vector-set! v 0 (+ (vector-ref v 0) 1)))))])
+  (while (<= (vector-ref v 0) n)
     (begin
       (print v)
-      (f)
-      (set! i (+ i 1)))))
+      (f))))
