30ea61a9743419e958ec844a181ffe80f97b0424
bmourad01 <Benjamin_Mourad@student.uml.edu>
2021-02-06 09:21:37 -0500

    fmt

--- examples/assign2.rkt
+++ examples/assign2.rkt
@@ -5,7 +5,7 @@
 
 (let ([counter (f 0)])
   (let* ([get (vector-ref counter 0)]
-        [inc (vector-ref counter 1)])
+         [inc (vector-ref counter 1)])
     (begin
       (inc)
       (get))))
